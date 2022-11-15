f134d537466fa02ffe01df876ba55f30dea3fb30
SimonMerlaud <simon.merlaud@gmail.com>
2022-11-15 11:59:32 +0100

    Ajout Benevole Type et Controller

---
+++ src/Controller/BenevoleController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Adresse;
+use App\Entity\Contact;
+use App\Form\BenevoleType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/benevole', name: 'benevole_')]
+class BenevoleController extends AbstractController
+{
+
+    #[Route('')]
+    public function index(): Response{
+        return $this->render('benevole/index.html.twig', [
+            'controller_name' => 'BenevoleController',
+        ]);
+    }
+
+    #[Route('/inscription', name: 'inscription_')]
+    public function inscriptionAction(EntityManagerInterface $em, Request $request): Response{
+
+        $benevole = new Contact();
+        $adresse = new Adresse();
+        $benevole->setAdresse($adresse);
+
+        $form = $this->createForm(BenevoleType::class, $benevole);
+        $form->add('send', SubmitType::class, ['label' => 'Valider']);
+        $form->handleRequest($request);
+        $benevole->setBenevole(true);
+
+
+        if($form->isSubmitted() && $form->isValid()){
+            //mettre le formulaire
+            dump($benevole);
+            $em->persist($benevole);
+            $em->persist($adresse);
+            $em->flush();
+
+            //faire un render sur une page pour dire que
+            //le user est bien inscrit
+
+            return $this->render('benevole/index.html.twig', [
+                'controller_name' => 'Bien inscrit',
+            ]);
+
+        }
+
+        return $this->render('benevole/form.html.twig', ['form' => $form->createView()]);
+
+
+    }
+
+
+}
---
+++ src/Form/BenevoleType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Contact;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class BenevoleType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom', TextType::class)
+            ->add('prenom', TextType::class)
+            ->add('mail', EmailType::class)
+            ->add('telephone', TelType::class)
+            ->add('proximite', ChoiceType::class,[
+                'choices' => [
+                    'Oui' => true,
+                    'Non' => false,
+                ]
+            ])
+            ->add('dateDisponibilite')
+            //Pouvoir mettre son adresse
+            ->add('adresse', AdresseType::class)
+
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Contact::class,
+        ]);
+    }
+}
