31ef5dcafe175067aacad7c68de55a3cb680cf89
SimonMerlaud <simon.merlaud@gmail.com>
2023-01-26 10:16:01 +0100

    Ajout export (Marche pour bénévole, pour magasin peut être)

--- src/Controller/BookingController.php
+++ src/Controller/BookingController.php
@@ -6,16 +6,15 @@ use App\Entity\Booking;
 use App\Entity\Lieux;
 use App\Form\BookingType;
 use App\Repository\BookingRepository;
-use App\Repository\ContactRepository;
-use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Validator\Constraints\Date;
+use Symfony\Component\Serializer\Encoder\CsvEncoder;
+use Symfony\Component\Serializer\Normalizer\DateTimeNormalizer;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
 
 #[Route(path: '/booking', name: 'booking')]
 class BookingController extends AbstractController
@@ -102,4 +101,38 @@ class BookingController extends AbstractController
         }
 
     }
+
+    #[Route(path: '/export/{mag}', name: '_export')]
+    public function export($mag): Response
+    {
+
+        if($this->isGranted('ROLE_BENEVOLE')){
+            $bookings = $this->getUser()->getContact()->getBookings();
+        }else{
+            $bookings = $mag->getBookings();
+        }
+        $bookingsExport = array();
+
+        foreach ($bookings as $booking){
+            if($this->isGranted('ROLE_BENEVOLE')){
+                $bookingsExport[] = ['Date de début' => $booking->getBeginAt(), 'Date de fin' => $booking->getEndAt(),'Titre' => $booking->getTitle()];
+            }else {
+                $bookingsExport[] = ['Date de début' => $booking->getBeginAt(), 'Date de fin' => $booking->getEndAt(), 'Titre' => $booking->getTitle()];
+            }
+        }
+
+        $normalizer = array(new DateTimeNormalizer(), new ObjectNormalizer());
+
+        $serializer = new Serializer($normalizer, [new CsvEncoder()]);// Les 2 lignes pour avoir la date dans le bon format
+        $context = [DateTimeNormalizer::FORMAT_KEY => 'Y-m-d H:i:s'];
+
+        $serializedBooking = $serializer->serialize($bookingsExport,'csv', $context);
+
+        $response = new Response($serializedBooking);
+        $response->headers->set('Content-Encoding', 'UTF-8');
+        $response->headers->set('Content-Type', 'text/csv; charset=UTF-8');
+        $response->headers->set('Content-Disposition', 'attachment; filename=export.csv');
+        return $response;
+    }
+
 }
